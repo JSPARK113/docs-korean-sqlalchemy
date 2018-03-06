51cebca284c11be2abd94e3f2bfbfc0f2b399e18
Joel Kim <kim.dohhyoung@gmail.com>
2018-03-06 12:22:31 +0900

    automatic commit

--- doc/build/conf.py
+++ doc/build/conf.py
@@ -31,7 +31,7 @@ needs_sphinx = '1.6.0'
 
 extensions = [
     'sphinx.ext.autodoc',
-#    'zzzeeksphinx',
+    #    'zzzeeksphinx',
     'changelog',
     'sphinx_paramlinks',
 ]
@@ -157,7 +157,16 @@ gettext_compact = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # html_theme = 'zzzeeksphinx'
+
 html_theme = "sphinx_rtd_theme"
+html_context = {
+    'github_user': 'VeranosTech',
+    'github_repo': 'docs-korean-sqlalchemy',
+    'github_version': 'docs-korean/',
+    'display_github': True,
+    'conf_py_path': 'docs/build/',
+    'source_suffix': '.rst',
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -170,11 +179,11 @@ html_theme = "sphinx_rtd_theme"
 # The style sheet to use for HTML and HTML Help pages. A file of that name
 # must exist either in Sphinx' static/ path, or in one of the custom paths
 # given in html_static_path.
-html_style = 'default.css'
+# html_style = 'default.css'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "%s %s 문서" % (project, version)
+# html_title = "%s %s 문서" % (project, version)
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 # html_short_title = None
@@ -191,11 +200,11 @@ html_title = "%s %s 문서" % (project, version)
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+# html_static_path = []
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-html_last_updated_fmt = '%m/%d/%Y %H:%M:%S'
+# html_last_updated_fmt = '%m/%d/%Y %H:%M:%S'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
@@ -211,7 +220,7 @@ html_last_updated_fmt = '%m/%d/%Y %H:%M:%S'
 # }
 
 # If false, no module index is generated.
-html_domain_indices = False
+# html_domain_indices = False
 
 # If false, no index is generated.
 # html_use_index = True
@@ -221,10 +230,10 @@ html_domain_indices = False
 
 # If true, the reST sources are included in the HTML build as _sources/<name>.
 # html_copy_source = True
-html_copy_source = False
+# html_copy_source = False
 
 # If true, links to the reST sources are added to the pages.
-html_show_sourcelink = True
+# html_show_sourcelink = True
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 # html_show_sphinx = True
